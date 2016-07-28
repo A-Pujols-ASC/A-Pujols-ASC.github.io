e654e721092b56a35704d81ec1bd0b5f6a00bd06
Hitsuga Aorusaki <apujols17@lawrenceville.org>
2016-07-28 09:49:42 -0400

    adding more features

---
+++ script.js
@@ -0,0 +1,14 @@
+//Makes pic larger/smaller if mouse is on/off
+function enlarge(pic){
+	pic.style.width="400px";  
+	pic.style.height="400px";
+}
+function minimize(pic){
+	pic.style.width="200px";
+	pic.style.height="200px";
+}
+//Changes text size with click
+function changeSize(){
+	var x = document.getElementById("coolbutton");
+	x.style.fontSize = "60px";
+};
