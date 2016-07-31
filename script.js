11a24fb4f84121956fa7b772d010eec777d4c03f
Hitsuga Aorusaki <apujols17@lawrenceville.org>
2016-07-31 19:18:18 -0400

    First major update

--- script.js
+++ script.js
@@ -1,14 +1,34 @@
 //Makes pic larger/smaller if mouse is on/off
+function setup(){
+  createCanvas(100, 100, WEBGL);
+}
+
+function draw(){
+  background(200);
+  rotateX(frameCount * 0.01);
+  rotateY(frameCount * 0.01);
+  box(200, 200, 200);
+}
+//Enlarges pic the mouse is over
 function enlarge(pic){
-	pic.style.width="400px";  
-	pic.style.height="400px";
+	pic.style.width="500px";  
+	pic.style.height="500px";
 }
 function minimize(pic){
 	pic.style.width="200px";
 	pic.style.height="200px";
 }
-//Changes text size with click
+//Changes text size with click (changes back if clicked again)
+var press = 0;
+
 function changeSize(){
 	var x = document.getElementById("coolbutton");
-	x.style.fontSize = "60px";
+	if (press== 0) {
+		x.style.fontSize = "60px";
+		press = 1;
+    } else {
+    	x.style.fontSize = "38px";
+    	press = 0;
+    }
+
 };
